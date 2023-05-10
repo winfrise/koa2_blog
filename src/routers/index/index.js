aaf5df9de0ee6bf4dd157d1be4a31a2ed1059b9c
winfrise <fanwenxiang326@163.com>
2023-05-10 18:08:45 +0800

    修改模板

--- src/routers/index/index.js
+++ src/routers/index/index.js
@@ -3,6 +3,11 @@ const router = new Router()
 
 const sqlApi = require('../../lib/db-utils')
 
+let menus 
+sqlApi.selectMenus().then(res => {
+    menus = res
+})
+
 const templates = ['default', 'college']
 const template = templates[1]
 
@@ -11,36 +16,35 @@ router.get('/', async ctx => {
 })
 
 router.get('/home', async ctx => {
-    const menus = await sqlApi.selectMenus()
     await ctx.render(`index/${template}/home/index`, { template, menus })
 })
 
 router.get('/category/list', async ctx => {
-    await ctx.render(`index/${template}/category/list`)
+    await ctx.render(`index/${template}/category/list`, { template, menus })
 })
 
 router.get('/article/list', async ctx => {
-    await ctx.render(`index/${template}/article/list`)
+    await ctx.render(`index/${template}/article/list`, { template, menus })
 })
 
 router.get('/article/details', async ctx => {
-    await ctx.render(`index/${template}/article/details`)
+    await ctx.render(`index/${template}/article/details`, { template, menus })
 })
 
 router.get('/uploads/list', async ctx => {
-    await ctx.render(`index/${template}/uploads/list`)
+    await ctx.render(`index/${template}/uploads/list`, { template, menus })
 })
 router.get('/uploads/details', async ctx => {
-    await ctx.render(`index/${template}/uploads/details`)
+    await ctx.render(`index/${template}/uploads/details`, { template, menus })
 })
 
 /**
  * 资源页
  */
 router.get('/resource/list', async ctx => {
-    await ctx.render(`index/${template}/resource/list`)
+    await ctx.render(`index/${template}/resource/list`, { template, menus })
 })
 router.get('/resource/details', async ctx => {
-    await ctx.render(`index/${template}/resource/details`)
+    await ctx.render(`index/${template}/resource/details`, { template, menus })
 })
 module.exports = router
